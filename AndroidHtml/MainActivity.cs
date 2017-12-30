358ff1a2316f1c36e4eea87e6ba323593d554bd0
Ryan Yang <Ryanyang99@hotmail.com>
2017-12-30 07:51:28 -0800

    First Commit

---
+++ AndroidHtml/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace AndroidHtml
+{
+    [Activity(Label = "Bruin-Locker", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+
+            SetContentView(Resource.Layout.Main);
+
+            WebView webView = FindViewById<WebView>(Resource.Id.LocalWebView);
+            webView.SetWebViewClient(new WebViewClient()); // stops request going to Web Browser
+            webView.Settings.JavaScriptEnabled = true;
+            webView.LoadUrl("https://script.google.com/macros/s/AKfycbzyTsXqaxirlzt_kznDKkU7kc6rObKLbYY4d1ZldvKYUhdjWfDb/exec");
+
+		}
+    }
+}
+
